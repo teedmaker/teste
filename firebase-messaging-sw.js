2aa621198d9769aab28d47108ddf64709829e225
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2023-01-10 16:02:48 -0300

    refactor: configura notificacoes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,50 +1,46 @@
-importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
-importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
-);
-// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
-importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
-);
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-    apiKey: "AIzaSyAYyws2NvvXfPqKRBQNoc2SifKCOA0-bQI",
-    authDomain: "testeapp-aa6a6.firebaseapp.com",
-    projectId: "testeapp-aa6a6",
-    storageBucket: "testeapp-aa6a6.appspot.com",
-    messagingSenderId: "409137549600",
-    appId: "1:409137549600:web:961e1db70326d33f438745"
-});
+// Import the functions you need from the SDKs you need
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js")
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js")
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-messaging
-  .requestPermission()
-  .then(function () {
-    console.log("Notification permission granted.");
-  })
-  .catch(function (err) {
-    console.log("Unable to get permission to notify.", err);
-  });
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYyws2NvvXfPqKRBQNoc2SifKCOA0-bQI",
+  authDomain: "testeapp-aa6a6.firebaseapp.com",
+  projectId: "testeapp-aa6a6",
+  storageBucket: "testeapp-aa6a6.appspot.com",
+  messagingSenderId: "409137549600",
+  appId: "1:409137549600:web:961e1db70326d33f438745"
+};
 
-messaging.setBackgroundMessageHandler(function(payload) {
-    // console.log(
-    //   "[firebase-messaging-sw.js] Received background message ",
-    //   payload,
-    // );
-    // Customize notification here
-    const notificationTitle = "Background Message Title";
-    const notificationOptions = {
-      body: "Background Message body.",
-      icon: "/itwonders-web-logo.png",
+const FIREBASE_VAPID_KEY = 'BHD2g9VbMlSCbN0vJBvVvP2DzaL59CYjMBQqvRU2OX9EH81NUpQA3PrrUPHjZrIduGPmBFWDkbA1ZMoEat1osCM';
+
+(async () => {
+  try {
+    initializeApp(firebaseConfig);
+
+    const messaging = getMessaging();
+
+    const getFirebaseToken = async () => {
+      try {
+        const currentToken = await getToken(messaging, { vapidKey: FIREBASE_VAPID_KEY });
+        if (!currentToken) {
+          console.log("No registration token available. Request permission to generate one.");
+        }
+      } catch (error) {
+        console.log("An error occurred while retrieving token. ", error);
+      }
     };
 
-    return self.registration.showNotification(
-      notificationTitle,
-      notificationOptions,
-    );
-});
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      await getFirebaseToken();
+    }
+  } catch (error) {
+    console.log("An error occurred while initializing firebase. ", error);
+    document.querySelector('#msg').innerHTML += 'Erro ao inicializar firebase: ' + error;
+  }
+})();
